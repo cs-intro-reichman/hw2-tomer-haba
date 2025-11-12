e1cd1038d46e4809e6d92dc88120d8d2c23ae61d
tomer-haba <tomer.haba@post.runi.ac.il>
2025-11-12 19:52:57 +0200

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,23 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	public static void main(String [] args) {
+		int N = Integer.parseInt(args[0]);
+
+		double sum = 0.0;
+		double sign = 1.0;
+
+		for(int i = 0; i < N ; i++) {
+			double k = 2 * i + 1.0;
+			double term = sign * (1.0 / k);
+			sum = sum + term;
+			sign = -sign;
+
+			double piApprox = 4.0 * sum;
+			
+			System.out.println("pi according to java: " + Math.PI );
+			System.out.println("pi, approximated: " + piApprox );
+		}
 	}
 }
+ 
+		
--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,33 @@ Great work boaz!
 
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+    public static void main(String[] args) {
+        
+        int N = Integer.parseInt(args[1]);
+        String S = args[0].toUpperCase(); 
+        
+        String SPECIALS = "AEFHILMNORSX"; 
+        
+        for (int i = 0; i < S.length(); i++) {
+            
+            char currentLetter = S.charAt(i); 
+            String article; 
+            
+            if (SPECIALS.indexOf(currentLetter) != -1) {
+                article = "an";
+            } else {
+                article = "a";
+            }
+            
+            System.out.println("Give me " + article + " " + currentLetter + ": " + currentLetter + "!");
         }
+        
+        System.out.println("What does that spell?"); 
+
+        String cheerLine = S + "!!!"; 
+        
+        for (int i = 0; i < N; i++) {
+            System.out.println(cheerLine);
+        }
+    }
 }
